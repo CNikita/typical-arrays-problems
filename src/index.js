2399d4ca2652eae07a990ae75dab5501f33ec9b2
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-09-30 18:07:41 +0300

    feat: add function avg

--- src/index.js
+++ src/index.js
@@ -16,5 +16,14 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined) {
+        return 0
+    } else if (array.length == 0) {
+        return 0
+    }
+    let sumArrayElement = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumArrayElement += array[i];
+    }
+    return sumArrayElement / array.length
 }
