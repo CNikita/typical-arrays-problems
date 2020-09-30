bcd61cdb0497543a8538196328f853ae3c911386
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-09-30 17:46:09 +0300

    feat: add function max

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxNumberInArray = Math.max.apply(null, array);
+    if (maxNumberInArray == -Infinity) {
+        maxNumberInArray = 0;
+    }
+    return maxNumberInArray
 }
 
 exports.avg = function avg (array) {
